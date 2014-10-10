6d775290fea97b54163dcce78e10dbb7ca9ee9eb
Tomas Spacek <tomspacek@gmail.com>
2014-10-10 20:28:50 +1100

    Handle active_admin asset files in plain JS and sass

--- lib/generators/active_admin_multi_upload/resource/resource_generator.rb
+++ lib/generators/active_admin_multi_upload/resource/resource_generator.rb
@@ -3,12 +3,35 @@ module ActiveAdminMultiUpload
     source_root File.expand_path('../templates', __FILE__)
 
     def add_js_assets
-      append_file "app/assets/javascripts/active_admin.js.coffee", "#= require jquery-fileupload\n"
-      append_file "app/assets/javascripts/active_admin.js.coffee", "#= require jquery-fileupload/vendor/tmpl\n"
+      js_filename = "app/assets/javascripts/active_admin.js"
+      coffee_filename = js_filename + ".coffee"
+
+      if File.exists? js_filename
+        # Plain JS
+        append_file js_filename, "//= require jquery-fileupload\n"
+        append_file js_filename, "//= require jquery-fileupload/vendor/tmpl\n"
+      elsif File.exists? coffee_filename
+        # Coffeescript
+        append_file coffee_filename, "#= require jquery-fileupload\n"
+        append_file coffee_filename, "#= require jquery-fileupload/vendor/tmpl\n"
+      else
+        raise "Couldn't find #{js_filename} or #{coffee_filename} to add JS to"
+      end
+
     end
 
     def add_css_assets
-      append_file "app/assets/stylesheets/active_admin.css.scss", '@import "jquery.fileupload-ui";'
+      scss_filename = "app/assets/stylesheets/active_admin.css.scss"
+      sass_filename = "app/assets/stylesheets/active_admin.css.sass"
+
+      if File.exists? scss_filename
+        append_file scss_filename, '@import "jquery.fileupload-ui";'
+      elsif File.exists? sass_filename
+        append_file sass_filename, '@import "jquery.fileupload-ui"'
+      else
+        raise "Couldn't find #{scss_filename} or #{sass_filename}. Need one of them to add CSS imports."
+      end
+
     end
 
     private
